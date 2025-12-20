230d27e0178bc0b3a442c888c4da9b3a60b280cf
xuliangzhan <xu_liangzhan@163.com>
2025-12-20 11:34:37 +0800

    update docs

--- ad/sponsors.js
+++ ad/sponsors.js
@@ -46,7 +46,7 @@ setTimeout(function () {
             }
             return [
               '<a href="' + (item ? item.url : '') + '" style="display: block !important;margin:8px 0 !important;width:200px !important;max-height:100px !important;text-align: center !important;border-radius: 2px !important;cursor: pointer !important;' + (style.a || '') + '" target="_blank">',
-                '<span style="display: block !important;font-size: 15px !important;padding: 8px 0 !important;' + (style.span || '') + '">', 
+                '<span style="display: block !important;font-size: 15px !important;' + (style.span || '') + '">', 
                   item.title || '', 
                 '</span>',
               '</a>'
