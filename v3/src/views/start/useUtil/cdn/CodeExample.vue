c932f79a6516831150d54eb91f1d3eb9f3126bb1
xuliangzhan <xu_liangzhan@163.com>
2026-01-14 11:06:09 +0800

    update docs

--- v3/src/views/start/useUtil/cdn/CodeExample.vue
+++ v3/src/views/start/useUtil/cdn/CodeExample.vue
@@ -11,7 +11,7 @@
       </template>
 
       <template #use>
-        <iframe :src="cdnUrl" style="display: block;width: 100%;height: 400px;border: 1px solid #000;"></iframe>
+        <iframe :src="cdnUrl" style="display: block;width: 100%;height: 300px;border: 1px solid #000;"></iframe>
         <code-render language="html" :code="cdnCode"></code-render>
       </template>
     </CodeLight>
