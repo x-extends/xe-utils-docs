15f174771b100ff39b698320b6260043883a70e5
xuliangzhan <xu_liangzhan@163.com>
2026-06-23 17:57:39 +0800

    update docs

--- v4/src/components/PageFooter.vue
+++ v4/src/components/PageFooter.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="page-footer">
-    <VxeLink href="http://beian.miit.gov.cn/" target="_blank" content="粤ICP备2022010374号-2">
+    <VxeLink href="http://beian.miit.gov.cn/" target="_blank" :content="aqCode">
       <template #icon>
-        <img :src="`${siteBaseUrl}/badge.png`" style="height: 14px;padding-bottom: 0.2em;">
+        <img :src="`${resBaseUrl}/badge.png`" style="height: 14px;padding-bottom: 0.2em;">
       </template>
     </VxeLink>
     <span style="margin-left: 20px;">
       <span>©</span>
-      <span><VxeLink status="primary" :href="siteBaseUrl" target="_blank">Vxe UI</VxeLink> 2018-{{serveTY}}</span>
+      <span><VxeLink status="primary" :href="siteBaseUrl" target="_blank">VXE</VxeLink> 2018-{{serveTY}}</span>
     </span>
   </div>
 </template>
@@ -18,11 +18,20 @@ import { useAppStore } from '@/store/app'
 
 const appStore = useAppStore()
 const serveTY = computed(() => appStore.serveTY)
+const resBaseUrl = computed(() => appStore.resBaseUrl)
 const siteBaseUrl = computed(() => appStore.siteBaseUrl)
+
+const aqCode = computed(() => {
+  if (location.origin && location.origin.indexOf('vxeui.com')) {
+    return '粤ICP备2022010374号'
+  }
+  return '粤ICP备2022010374号'
+})
 </script>
 
 <style lang="scss">
 .page-footer {
+  padding-top: 4em;
   text-align: center;
 }
 </style>
